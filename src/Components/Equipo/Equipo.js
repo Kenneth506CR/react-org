bd27042cb86e9551ff70b1f0aa93bf161e008e12
Kenneth Rodríguez <105812356+Kenneth506CR@users.noreply.github.com>
2023-09-12 14:23:58 -0600

    creando espacios de equipos

--- src/Components/Equipo/Equipo.js
+++ src/Components/Equipo/Equipo.js
@@ -2,14 +2,12 @@ import "./Equipo.css";
 import Colaborador from "../Colaborador/Colaborador.js";
 
 const Equipo = (props) => {
+  //Destructuración
+  const { colorPrimario, colorSecundario, titulo } = props.datos;
+
   return (
-    <section
-      className="equipo"
-      style={{ backgroundColor: props.datos.colorSecundario }}
-    >
-      <h3 style={{ borderColor: props.datos.colorPrimario }}>
-        key={props.datos.titulo}
-      </h3>
+    <section className="equipo" style={{ backgroundColor: colorSecundario }}>
+      <h3 style={{ borderColor: colorPrimario }}>{titulo}</h3>
       <Colaborador />
       <div className="colaboradores"></div>
     </section>
